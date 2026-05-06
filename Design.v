a663a9b00bff88618688e22e88c09240e001c4d3
Meghavs124 <meghavs777@gmail.com>
2026-05-06 11:44:06 +0530

    Modified ALU

--- Design.v
+++ Design.v
@@ -25,142 +25,142 @@ reg [1:0] count;
       begin
          if(RST)               
           begin
-            RES=8'b00000000;
-            COUT=1'b0;
-            OFLOW=1'b0;
-            G=1'b0;
-            E=1'b0;
-            L=1'b0;
-            ERR=1'b0;
-            count=1'b0;
+            RES<=8'b00000000;
+            COUT<=1'b0;
+            OFLOW<=1'b0;
+            G<=1'b0;
+            E<=1'b0;
+            L<=1'b0;
+            ERR<=1'b0;
+            count<=1'b0;
           end
       else if(CE)
        begin
           if(MODE)        
          begin
-           RES=8'b00000000;
-           COUT=1'b0;
-           OFLOW=1'b0;
-           G=1'b0;
-           E=1'b0;
-           L=1'b0;
-           ERR=1'b0;
+           RES<=8'b00000000;
+           COUT<=1'b0;
+           OFLOW<=1'b0;
+           G<=1'b0;
+           E<=1'b0;
+           L<=1'b0;
+           ERR<=1'b0;
            
           case(CMD) 
-          //CMD 0 unsigned add            
+         
            4'b0000:             
             begin  
             if(INP_VALID==2'b11)
              begin           
-              RES=OPA+OPB;
-              COUT=RES[N]?1:0;
+              RES<=OPA+OPB;
+              COUT<=RES[N]?1:0;
              end 
             else
-            ERR=1'b1;
+            ERR<=1'b1;
             end
             
-               //CMD 1 unsigned sub
+            
 	      4'b0001:             
             begin
              if(INP_VALID==2'b11)
              begin
-              OFLOW=(OPA<OPB)?1:0;
-              RES=OPA-OPB;
+              OFLOW<=(OPA<OPB)?1:0;
+              RES<=OPA-OPB;
              end
              else
-               ERR=1'b1;
+               ERR<=1'b1;
             end 
             
-               //CMD 2 add with cin
+           
            4'b0010:             
             begin
             if(INP_VALID==2'b11)
             begin
-             RES=OPA+OPB+CIN;
-             COUT=RES[N]?1:0;
+             RES<=OPA+OPB+CIN;
+             COUT<=RES[N]?1:0;
             end
             else
-             ERR=1'b1;
+             ERR<=1'b1;
             end
             
-            //CMD 3 sub with cin
+            
            4'b0011:             
            begin
              if(INP_VALID==2'b11)
               begin
-               OFLOW=(OPA<OPB)?1:0;
-               RES=OPA-OPB-CIN;
+               OFLOW<=(OPA<OPB)?1:0;
+               RES<=OPA-OPB-CIN;
               end
              else
-              ERR=1'b1;
+              ERR<=1'b1;
            end
            
-           //CMD 4 increment_A
+          
            4'b0100:
            begin
             if(INP_VALID==2'b01 || INP_VALID==2'b11)
-             RES=OPA+1;  
+             RES<=OPA+1;  
             else
-             ERR=1'b1;
+             ERR<=1'b1;
            end 
                 
-          //cmd 5 decrement A      
+          
            4'b0101:
            begin   
             if(INP_VALID==2'b01 || INP_VALID==2'b11)
-             RES=OPA-1; 
+             RES<=OPA-1; 
             else
-             ERR=1'b1;
+             ERR<=1'b1;
            end 
            
-           //cmd 6 increment B
+          
            4'b0110: 
            begin
             if(INP_VALID==2'b10 || INP_VALID==2'b11)
-                RES=OPB+1; 
+                RES<=OPB+1; 
             else
-              ERR=1'b1;
+              ERR<=1'b1;
            end 
            
-           //cmd 7 decrement B
+           
            4'b0111:
            begin
             if(INP_VALID==2'b10 || INP_VALID==2'b11)
-                RES=OPB-1;
+                RES<=OPB-1;
             else
-              ERR=1'b1;
+              ERR<=1'b1;
            end 
            
-           //cmd 8 cmpare   
+           
            4'b1000:              
            begin
-            RES=8'b00000000;
+            RES<=8'b00000000;
            if(INP_VALID==2'b11)
             begin
              if(OPA==OPB)
               begin
-               E=1'b1;
-               G=1'b0;
-               L=1'b0;
+               E<=1'b1;
+               G<=1'b0;
+               L<=1'b0;
               end
              else if(OPA>OPB)
               begin
-               E=1'b0;
-               G=1'b1;
-               L=1'b0;
+               E<=1'b0;
+               G<=1'b1;
+               L<=1'b0;
               end
              else 
               begin
-               E=1'b0;
-               G=1'b0;
-               L=1'b1;
+               E<=1'b0;
+               G<=1'b0;
+               L<=1'b1;
               end
             end
            else
-            ERR=1'b1;
+            ERR<=1'b1;
            end
            
-           //cmd 9 mul res at  3rd clk cycle
+          
            
            4'b1001:
            begin
@@ -169,25 +169,25 @@ reg [1:0] count;
            if (count == 0) 
 		   begin
             temp1 = 0;
-             count = 1;
+             count <= 1;
             end
           else if(count==1)
            begin
-           temp1=(OPA+1)*(OPB+1);
-           count=count+1;
+           temp1<=(OPA+1)*(OPB+1);
+           count<=count+1;
            end
-          // for(count=1;count<=3;count=count+1)
+        
            else if(count==2)
            begin
-           RES = temp1;
-           count=0;
+           RES <= temp1;
+           count<=0;
            end
            end
            else
-           ERR=1'b1;
+           ERR<=1'b1;
            end
            
-           //cmd 10 shift and mul
+           
            4'b1010:
            begin
            if(INP_VALID==2'b11)
@@ -195,177 +195,177 @@ reg [1:0] count;
            if (count == 2'd0) 
 		   begin
              temp1 = 0;
-             count = 1;
+             count <= 1;
            end
            else if(count==1) 
 		   begin
-           temp1=(OPA<<1)*OPB;
-           count=count+1;
+           temp1<=(OPA<<1)*OPB;
+           count<=count+1;
            end
-          // for(count=1;count<=3;count=count+1)
+          
            else if(count==2)
 		   begin
-           RES=temp1;
-           count=0;
+           RES<=temp1;
+           count<=0;
            end
            end
            else
-           ERR=1'b1;
+           ERR<=1'b1;
            end
            
-           //cmd 11signed addition and comparision
+           
            4'b1011:
            begin
             if(INP_VALID==2'b11)
              begin
-              RES=$signed(OPA) + $signed(OPB);
-              //COUT=RES[8]?1:0;
-              OFLOW=(OPA[N-1]==OPB[N-1]&& RES[N-1]!=OPA[N-1]);
+              RES<=$signed(OPA) + $signed(OPB);
+              
+              OFLOW<=(OPA[N-1]==OPB[N-1]&& RES[N-1]!=OPA[N-1]);
              end 
              if (INP_VALID==2'b11) 
              begin
                 if($signed(OPA)==$signed(OPB))
                   begin
-                      E=1'b1;
-                      G=1'b0;
-                      L=1'b0;
+                      E<=1'b1;
+                      G<=1'b0;
+                      L<=1'b0;
                   end 
                 else if($signed(OPA)>$signed(OPB))
                    begin
-                     E=1'b0;
-                     G=1'b1;
-                     L=1'b0;
+                     E<=1'b0;
+                     G<=1'b1;
+                     L<=1'b0;
                   end
                 else 
                    begin
-                    E=1'b0;
-                    G=1'b0;
-                    L=1'b1;
+                    E<=1'b0;
+                    G<=1'b0;
+                    L<=1'b1;
                    end
              end        
             else
-               ERR=1'b1;
+               ERR<=1'b1;
             end 
             
-            //cmd 12 signed sub and compare
+            
              4'b1100:
            begin
             if(INP_VALID==2'b11)
              begin
-              RES=$signed(OPA) - $signed(OPB);
-              //COUT=RES[8]?1:0;
-              OFLOW=(OPA[N-1]!=OPB[N-1]&& RES[N-1]!=OPA[N-1]);
+              RES<=$signed(OPA) - $signed(OPB);
+              
+              OFLOW<=(OPA[N-1]!=OPB[N-1]&& RES[N-1]!=OPA[N-1]);
               end 
              if (INP_VALID==2'b11) 
              begin
                 if($signed(OPA)==$signed(OPB))
                   begin
-                      E=1'b1;
-                      G=1'b0;
-                      L=1'b0;
+                      E<=1'b1;
+                      G<=1'b0;
+                      L<=1'b0;
                   end
                 else if($signed(OPA)>$signed(OPB))
                    begin
-                     E=1'b0;
-                     G=1'b1;
-                     L=1'b0;
+                     E<=1'b0;
+                     G<=1'b1;
+                     L<=1'b0;
                   end
                 else 
                    begin
-                    E=1'b0;
-                    G=1'b0;
-                    L=1'b1;
+                    E<=1'b0;
+                    G<=1'b0;
+                    L<=1'b1;
                    end
              end        
             else
-               ERR=1'b1;
+               ERR<=1'b1;
             end 
             
            
            default:
             begin
-            RES=8'b00000000;
-            COUT=1'b0;
-            OFLOW=1'b0;
-            G=1'b0;
-            E=1'b0;
-            L=1'b0;
-            ERR=1'b0;
+            RES<=8'b00000000;
+            COUT<=1'b0;
+            OFLOW<=1'b0;
+            G<=1'b0;
+            E<=1'b0;
+            L<=1'b0;
+            ERR<=1'b0;
            end
           endcase
          end
  
         else          
         begin 
-           RES=8'b00000000;
-           COUT=1'b00;
-           OFLOW=1'b0;
-           G=1'b0;
-           E=1'b0;
-           L=1'b0;
-           ERR=1'b0;
+           RES<=8'b00000000;
+           COUT<=1'b00;
+           OFLOW<=1'b0;
+           G<=1'b0;
+           E<=1'b0;
+           L<=1'b0;
+           ERR<=1'b0;
            
          case(INP_VALID)
-         2'b00: ERR=1'b1;
+         2'b00: ERR<=1'b1;
          
          2'b01: begin
          case(CMD)
-         4'b0110:RES={1'b0,~OPA}; 
-         4'b1000:RES={1'b0,OPA>>1};      
-         4'b1001:RES={1'b0,OPA<<1}; 
-         default : ERR=1'b1;
+         4'b0110:RES<={1'b0,~OPA}; 
+         4'b1000:RES<={1'b0,OPA>>1};      
+         4'b1001:RES<={1'b0,OPA<<1}; 
+         default : ERR<=1'b1;
          endcase 
          end
          
          2'b10: begin
          case(CMD)
-         4'b0111:RES={1'b0,~OPB};
-         4'b1010:RES={1'b0,OPB>>1};      
-         4'b1011:RES={1'b0,OPB<<1};  
-         default : ERR=1'b1;
+         4'b0111:RES<={1'b0,~OPB};
+         4'b1010:RES<={1'b0,OPB>>1};      
+         4'b1011:RES<={1'b0,OPB<<1};  
+         default : ERR<=1'b1;
          endcase
          end
          
          2'b11: begin
          case(CMD)
-         4'b0000:RES={1'b0,OPA&OPB};     
-         4'b0001:RES={1'b0,~(OPA&OPB)};  
-         4'b0010:RES={1'b0,OPA|OPB};     
-         4'b0011:RES={1'b0,~(OPA|OPB)}; 
-         4'b0100:RES={1'b0,OPA^OPB}; 
-         4'b0101:RES={1'b0,~(OPA^OPB)};
+         4'b0000:RES<={1'b0,OPA&OPB};     
+         4'b0001:RES<={1'b0,~(OPA&OPB)};  
+         4'b0010:RES<={1'b0,OPA|OPB};     
+         4'b0011:RES<={1'b0,~(OPA|OPB)}; 
+         4'b0100:RES<={1'b0,OPA^OPB}; 
+         4'b0101:RES<={1'b0,~(OPA^OPB)};
          4'b1100:                        
              begin             
               if(OPB[N-1:3]!=0)
-              ERR=1'b1;
+              ERR<=1'b1;
              else
               begin
-               shift = OPB % N;   
-               rot_temp = (OPA << shift) | (OPA >> (N - shift));
-               RES = rot_temp;
+               shift <= OPB % N;   
+               rot_temp <= (OPA << shift) | (OPA >> (N - shift));
+               RES <= rot_temp;
              end 
              end
          4'b1101: 
          begin
          if(OPB[N-1:3]!=0)
-         ERR=1'b1;
+         ERR<=1'b1;
          else
          begin
-          shift = OPB % N;
-          rot_temp = (OPA >> shift) | (OPA << (N - shift));
-          RES = rot_temp;
+          shift <= OPB % N;
+          rot_temp <= (OPA >> shift) | (OPA << (N - shift));
+          RES <= rot_temp;
          end
         end
              
         default:    
                begin
-               RES=8'b00000000;
-               COUT=1'b0;
-               OFLOW=1'b0;
-               G=1'b0;
-               E=1'b0;
-               L=1'b0;
-               ERR=1'b0;
+               RES<=8'b00000000;
+               COUT<=1'b0;
+               OFLOW<=1'b0;
+               G<=1'b0;
+               E<=1'b0;
+               L<=1'b0;
+               ERR<=1'b0;
                end
           endcase
      end
@@ -375,4 +375,3 @@ reg [1:0] count;
    end
  end
  endmodule  
-  
